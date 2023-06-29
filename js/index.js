0018185d14b5f1a65eac047aaf55254ffcebf091
leorjr <l.grodriguesjr@gmail.com>
2023-06-28 21:39:37 -0300

    feat: implementado lista dinamica de medicamentos

--- js/index.js
+++ js/index.js
@@ -1,31 +1,3 @@
-// const header = document.querySelector('header')
-// const nav = `
-//     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-//         <div class="container-fluid">
-//             <h1 class="navbar-brand">
-//                 <a href="./index.html">Farmácia <span>Z</span></a>
-//             </h1>
-//         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//             <span class="navbar-toggler-icon"></span>
-//         </button>
-//         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-//             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-//             <li class="nav-item">
-//                 <a class="nav-link active" aria-current="page" href="index.html">Todos os medicamentos</a>
-//             </li>
-//             <li class="nav-item">
-//                 <a class="nav-link" href="contato.html">Fale conosco</a>
-//             </li>
-//             </ul>
-//         </div>
-//         </div>
-//     </nav>
-// `
-
-{/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button> */}
-
 const body = document.querySelector('body')
 const header = document.createElement('header')
 body.appendChild(header)
@@ -60,5 +32,49 @@ div.innerHTML = `
         </ul>
     </div>
 `
+
+const medicamentos = [
+    {
+        img: './images/comprimidos.png',
+        title: 'Comprimidos',
+        valor: 'R$ 100,00'
+    },
+    {
+        img: './images/pomada.jpg',
+        title: 'Pomada',
+        valor: 'R$ 50,00'
+    },
+    {
+        img: './images/colirio.jpg',
+        title: 'Colírio',
+        valor: 'R$ 10,00'
+    },
+    {
+        img: './images/injecao.jpg',
+        title: 'Injeção',
+        valor: 'R$ 15,00'
+    }
+]
+
+const secaoListagemMedicamentos = document.querySelector('section#listagem')
+const lista = secaoListagemMedicamentos.querySelector('ul')
+
+
+medicamentos.forEach(medicamento =>{
+    const itemLista = document.createElement('li')
+
+    itemLista.innerHTML = `
+    <div class="card">
+        <img src=${medicamento.img} class="card-img-top" alt=${medicamento.title}>
+        <div class="card-body">
+        <h5 class="card-title">${medicamento.title}</h5>
+        <p class="card-text">${medicamento.valor}</p>
+            <a href="#" class="btn btn-warning">adicionar</a>
+        </div>
+    </div>
+    `
+
+    lista.appendChild(itemLista)
+})
 
 
