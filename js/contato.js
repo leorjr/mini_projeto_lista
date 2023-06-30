d6163b31926f4038a32db5958e9b77ce02bae037
leorjr <l.grodriguesjr@gmail.com>
2023-06-29 21:52:30 -0300

    feat: implementado açoes do formulário

---
+++ js/contato.js
@@ -0,0 +1,18 @@
+const formSelect = document.querySelector('.form-select')
+const assuntos = ['duvidas', 'consultas', 'orçamentos', 'reclamações']
+
+for(let assunto of assuntos){
+    const selectOption = document.createElement('option')
+    formSelect.appendChild(selectOption)
+    selectOption.setAttribute('value', assunto)
+    selectOption.innerText = assunto
+}
+
+const button = document.querySelector('.btn-primary')
+const inputName = document.querySelector('#nome')
+
+button.addEventListener('click', (e)=>{
+    e.preventDefault()
+    console.log(`formulário de ${inputName.value} submetido com sucesso!`)
+    window.alert(`formulário de ${inputName.value} submetido com sucesso!`)
+})
